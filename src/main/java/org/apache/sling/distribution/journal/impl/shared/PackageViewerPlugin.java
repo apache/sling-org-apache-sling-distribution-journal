95aff37fbe28226b0d0ee327d2ea529397223b61
Christian Schneider <cschneid@adobe.com>
2019-11-28 11:36:47 +0100

    Fix warning

--- src/main/java/org/apache/sling/distribution/journal/impl/shared/PackageViewerPlugin.java
+++ src/main/java/org/apache/sling/distribution/journal/impl/shared/PackageViewerPlugin.java
@@ -25,7 +25,6 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.Servlet;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
