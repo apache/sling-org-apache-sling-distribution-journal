429f61f6407e92886f150f2c8c3ce5376f016779
Christian Schneider <cschneid@adobe.com>
2020-07-06 15:12:11 +0200

    SLING-9259 - Fixes for release

--- src/main/java/org/apache/sling/distribution/journal/impl/precondition/Precondition.java
+++ src/main/java/org/apache/sling/distribution/journal/impl/precondition/Precondition.java
@@ -18,8 +18,6 @@
  */
 package org.apache.sling.distribution.journal.impl.precondition;
 
-import java.util.concurrent.TimeoutException;
-
 /**
  * Extension point for checking if a package can be processed by a subscriber.
  */
@@ -27,11 +25,6 @@ public interface Precondition {
 
     /**
      * Checks if a package can be processed
-     * @param pkgOffset the offset of the package
-     * @param timeoutSeconds max seconds to wait until returning
-     * @throws TimeoutException if the timeout expired without being able to determine status
-     * @throws InterruptedException if the thread was interrupted and should shut down
-     * @return true if the package can be processed; otherwise it returns false.
      */
     Decision canProcess(String subAgentName, long pkgOffset);
 
