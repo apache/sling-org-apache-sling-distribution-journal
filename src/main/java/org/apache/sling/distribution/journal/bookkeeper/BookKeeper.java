cb706c58ecd5114086bd221d0dbc0a18d159263b
tmaret <tmaret@adobe.com>
2021-06-22 14:29:48 +0200

    SLING-10527 - Log imported instead of importing package

--- src/main/java/org/apache/sling/distribution/journal/bookkeeper/BookKeeper.java
+++ src/main/java/org/apache/sling/distribution/journal/bookkeeper/BookKeeper.java
@@ -149,7 +149,7 @@ public class BookKeeper implements Closeable {
      * once, thanks to the order in which the content updates are applied.
      */
     public void importPackage(PackageMessage pkgMsg, long offset, long createdTime) throws DistributionException {
-        log.info("Importing distribution package {} at offset={}", pkgMsg, offset);
+        log.debug("Importing distribution package {} at offset={}", pkgMsg, offset);
         addPackageMDC(pkgMsg);
         try (Timer.Context context = distributionMetricsService.getImportedPackageDuration().time();
                 ResourceResolver importerResolver = getServiceResolver(SUBSERVICE_IMPORTER)) {
@@ -169,6 +169,7 @@ public class BookKeeper implements Closeable {
              
             Event event = new ImportedEvent(pkgMsg, config.getSubAgentName()).toEvent();
             eventAdmin.postEvent(event);
+            log.info("Imported distribution package {} at offset={}", pkgMsg, offset);
         } catch (DistributionException | LoginException | IOException | RuntimeException | ImportPostProcessException e) {
             failure(pkgMsg, offset, e);
         } finally {
