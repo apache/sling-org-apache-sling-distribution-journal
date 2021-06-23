b2a97bc136f111e7ff1d6e776251eac520536e03
Timothee Maret <tmaret@apache.org>
2021-06-23 12:11:49 +0200

    SLING-10528 - Reject large packages in the distribution publisher (#75)

--- src/test/java/org/apache/sling/distribution/journal/bookkeeper/BookKeeperTest.java
+++ src/test/java/org/apache/sling/distribution/journal/bookkeeper/BookKeeperTest.java
@@ -18,16 +18,27 @@
  */
 package org.apache.sling.distribution.journal.bookkeeper;
 
+import static java.lang.System.currentTimeMillis;
+import static java.util.Collections.singletonList;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.util.UUID;
 import java.util.function.Consumer;
 
 import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.PersistenceException;
 import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.commons.metrics.Counter;
+import org.apache.sling.commons.metrics.Histogram;
+import org.apache.sling.commons.metrics.Meter;
+import org.apache.sling.commons.metrics.Timer;
 import org.apache.sling.distribution.ImportPostProcessor;
+import org.apache.sling.distribution.common.DistributionException;
 import org.apache.sling.distribution.journal.messages.LogMessage;
+import org.apache.sling.distribution.journal.messages.PackageMessage;
 import org.apache.sling.distribution.journal.messages.PackageStatusMessage;
 import org.apache.sling.distribution.journal.shared.DistributionMetricsService;
 import org.apache.sling.distribution.packaging.DistributionPackageBuilder;
@@ -43,6 +54,8 @@ import org.osgi.service.event.EventAdmin;
 public class BookKeeperTest {
 
     private static final int COMMIT_AFTER_NUM_SKIPPED = 10;
+
+    private static final String PUB_AGENT_NAME = "pubAgent";
 
     private ResourceResolverFactory resolverFactory = new MockResourceResolverFactory();
 
@@ -71,6 +84,20 @@ public class BookKeeperTest {
 
     @Before
     public void before() {
+        when(distributionMetricsService.getFailedPackageImports())
+                .thenReturn(mock(Meter.class));
+        when(distributionMetricsService.getImportedPackageDuration())
+                .thenReturn(mock(Timer.class));
+        when(distributionMetricsService.getImportedPackageSize())
+                .thenReturn(mock(Histogram.class));
+        when(distributionMetricsService.getPackageDistributedDuration())
+                .thenReturn(mock(Timer.class));
+        when(distributionMetricsService.getImportPostProcessRequest())
+                .thenReturn(mock(Counter.class));
+        when(distributionMetricsService.getImportPostProcessDuration())
+                .thenReturn(mock(Timer.class));
+        when(distributionMetricsService.getImportPostProcessSuccess())
+                .thenReturn(mock(Counter.class));
         BookKeeperConfig bkConfig = new BookKeeperConfig("subAgentName", "subSlingId", true, 10, PackageHandling.Extract, "package");
         bookKeeper = new BookKeeper(resolverFactory, distributionMetricsService, packageHandler, eventAdmin, sender, logSender, bkConfig,
             importPostProcessor);
@@ -90,6 +117,30 @@ public class BookKeeperTest {
             bookKeeper.skipPackage(c);
             assertThat(bookKeeper.loadOffset(), equalTo(20L));
         }
+    }
+
+    @Test
+    public void testPackageImport() throws DistributionException {
+        try {
+            bookKeeper.importPackage(buildPackageMessage(), 10, currentTimeMillis());
+        } finally {
+            assertThat(bookKeeper.getRetries(PUB_AGENT_NAME), equalTo(0));
+        }
+    }
+
+    PackageMessage buildPackageMessage() {
+        PackageMessage msg = mock(PackageMessage.class);
+        when(msg.getPkgLength())
+                .thenReturn(100L);
+        when(msg.getPubAgentName())
+                .thenReturn(PUB_AGENT_NAME);
+        when(msg.getReqType())
+                .thenReturn(PackageMessage.ReqType.ADD);
+        when(msg.getPaths())
+                .thenReturn(singletonList("/content"));
+        when(msg.getPkgId())
+                .thenReturn(UUID.randomUUID().toString());
+        return msg;
     }
 
 }
