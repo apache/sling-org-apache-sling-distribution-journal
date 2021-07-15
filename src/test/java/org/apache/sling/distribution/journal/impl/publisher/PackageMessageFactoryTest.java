1a0c39802da4647224ab9e364fb643e417c1d094
Timothee Maret <tmaret@apache.org>
2021-07-15 14:09:24 +0200

    SLING-10617 - Allow to configure the package size to reject (#85)

--- src/test/java/org/apache/sling/distribution/journal/impl/publisher/PackageMessageFactoryTest.java
+++ src/test/java/org/apache/sling/distribution/journal/impl/publisher/PackageMessageFactoryTest.java
@@ -19,25 +19,35 @@
 package org.apache.sling.distribution.journal.impl.publisher;
 
 import java.io.ByteArrayInputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.distribution.DistributionRequest;
 import org.apache.sling.distribution.SimpleDistributionRequest;
 import org.apache.sling.distribution.common.DistributionException;
+import org.apache.sling.distribution.journal.BinaryStore;
 import org.apache.sling.distribution.packaging.DistributionPackage;
 import org.apache.sling.distribution.packaging.DistributionPackageBuilder;
+import org.apache.sling.distribution.packaging.DistributionPackageInfo;
+import org.apache.sling.settings.SlingSettingsService;
 import org.apache.sling.testing.resourceresolver.MockResourceResolverFactory;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static java.util.Collections.emptyMap;
+import static java.util.Collections.singletonMap;
 import static org.apache.sling.distribution.DistributionRequestType.ADD;
-import static org.apache.sling.distribution.journal.impl.publisher.PackageMessageFactory.MAX_PACKAGE_SIZE;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.any;
+import static org.osgi.util.converter.Converters.standardConverter;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PackageMessageFactoryTest {
@@ -50,8 +60,15 @@ public class PackageMessageFactoryTest {
     @Mock
     private DistributionPackage distributionPackage;
 
+    @Mock
+    private SlingSettingsService slingSettingsService;
+
+    @Mock
+    private BinaryStore binaryStore;
+
     private final ResourceResolverFactory resolverFactory = new MockResourceResolverFactory();
 
+    @InjectMocks
     private final PackageMessageFactory factory = new PackageMessageFactory();
 
 
@@ -59,14 +76,42 @@ public class PackageMessageFactoryTest {
     public void before() throws DistributionException {
         when(packageBuilder.createPackage(any(ResourceResolver.class), any(DistributionRequest.class)))
                 .thenReturn(distributionPackage);
+        when(slingSettingsService.getSlingId())
+                .thenReturn(UUID.randomUUID().toString());
+
+        when(distributionPackage.getInfo())
+                .thenReturn(buildPackageInfo());
     }
 
     @Test(expected = DistributionException.class)
     public void testAddPkgLengthTooLarge() throws Exception {
+        PackageFactoryConfiguration config = standardConverter()
+                .convert(singletonMap("maxPackageSize", 1000))
+                .to(PackageFactoryConfiguration.class);
+        factory.activate(config);
         when(distributionPackage.createInputStream())
-                .thenReturn(new ByteArrayInputStream(new byte[(int)MAX_PACKAGE_SIZE + 1]));
+                .thenReturn(new ByteArrayInputStream(new byte[(int)config.maxPackageSize() + 1]));
         DistributionRequest request = new SimpleDistributionRequest(ADD, "/some/path");
         factory.create(packageBuilder, resolverFactory.getServiceResourceResolver(null), PUB_AGENT_NAME, request);
+    }
+
+    @Test
+    public void testAddPkgNoLimit() throws Exception {
+        PackageFactoryConfiguration config = standardConverter()
+                .convert(emptyMap())
+                .to(PackageFactoryConfiguration.class);
+        factory.activate(config);
+        when(distributionPackage.createInputStream())
+                .thenReturn(new ByteArrayInputStream(new byte[10_000_000]));
+        DistributionRequest request = new SimpleDistributionRequest(ADD, "/some/path");
+        assertNotNull(factory.create(packageBuilder, resolverFactory.getServiceResourceResolver(null), PUB_AGENT_NAME, request));
+    }
+
+    private DistributionPackageInfo buildPackageInfo() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(DistributionPackageInfo.PROPERTY_REQUEST_PATHS, "/some/path");
+        props.put(DistributionPackageInfo.PROPERTY_REQUEST_DEEP_PATHS, "/some/path");
+        return new DistributionPackageInfo("packageType", props);
     }
 
 }
