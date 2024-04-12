f0b3c5663cb326c41993f405a24211bd997e90da
Christian Schneider <cschneid@adobe.com>
2024-04-12 08:24:33 +0200

    SLING-12292 - Add support for tags

---
+++ src/main/java/org/apache/sling/distribution/journal/metrics/TaggedMetrics.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.sling.distribution.journal.metrics;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class TaggedMetrics {
+    private static Logger log = LoggerFactory.getLogger(TaggedMetrics.class);
+    
+    private TaggedMetrics() {
+    }
+
+    public static String getMetricName(String metricName, List<Tag> tags) {
+        String metric = tags.stream()
+                .map(tag -> ";" + tag.getKey() + "=" + tag.getValue())
+                .collect(Collectors.joining("", metricName, ""));
+        log.debug("metric={}", metric);
+        return metric;
+    }
+}
